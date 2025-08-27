4c91e4531a3daa8bcd396d217d42683935c5a76d
chenbiao <149027310@qq.com>
2025-08-27 18:27:52 +0800

    up

--- cron_test.go
+++ cron_test.go
@@ -2,14 +2,11 @@ package dgcron
 
 import (
 	"fmt"
-	dgctx "github.com/darwinOrg/go-common/context"
-	dglock "github.com/darwinOrg/go-dlock"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/rolandhe/daog"
-	"github.com/rolandhe/saber/gocc"
-	"log"
 	"testing"
 	"time"
+
+	dgctx "github.com/darwinOrg/go-common/context"
+	"github.com/rolandhe/saber/gocc"
 )
 
 func TestAddJob(t *testing.T) {
@@ -20,29 +17,6 @@ func TestAddJob(t *testing.T) {
 	})
 
 	time.Sleep(5 * time.Second)
-}
-
-func TestAddJobWithLock(t *testing.T) {
-	conf := &daog.DbConf{
-		DbUrl:    "root:12345678@tcp(localhost:3306)/whale?charset=utf8mb4&loc=Local&interpolateParams=true&parseTime=true&timeout=1s&readTimeout=2s&writeTimeout=2s",
-		Size:     200,
-		Life:     50,
-		IdleCons: 1200,
-		IdleTime: 3600,
-		LogSQL:   true,
-	}
-	db, err := daog.NewDatasource(conf)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	cron := NewAndStart(dglock.NewDbLocker(db))
-	defer cron.Stop()
-	cron.AddJobWithLock("TestJob", "*/1 * * * * ?", 5000, func(ctx *dgctx.DgContext) {
-		fmt.Println(time.Now().UnixMilli())
-	})
-
-	time.Sleep(10 * time.Second)
 }
 
 func TestAddFixDurationJob(t *testing.T) {
--- go.mod
+++ go.mod
@@ -6,20 +6,20 @@ require (
 	github.com/darwinOrg/go-common v0.2.9
 	github.com/darwinOrg/go-dlock v0.0.5
 	github.com/darwinOrg/go-logger v0.0.14
-	github.com/go-sql-driver/mysql v1.9.3
 	github.com/panjf2000/ants/v2 v2.11.3
 	github.com/robfig/cron/v3 v3.0.1
-	github.com/rolandhe/daog v0.0.41
 	github.com/rolandhe/saber v0.0.5
 )
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/darwinOrg/daog-ext v0.0.14 // indirect
+	github.com/go-sql-driver/mysql v1.9.3 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/natefinch/lumberjack v2.0.0+incompatible // indirect
+	github.com/rolandhe/daog v0.0.41 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/sync v0.16.0 // indirect
