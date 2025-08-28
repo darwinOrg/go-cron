0beb0df4f516c1cef054c0fd48a97c09e1bde534
chenbiao <149027310@qq.com>
2025-08-28 17:25:47 +0800

    redis lock

--- cron_test.go
+++ cron_test.go
@@ -6,6 +6,8 @@ import (
 	"time"
 
 	dgctx "github.com/darwinOrg/go-common/context"
+	dglock "github.com/darwinOrg/go-dlock"
+	redisdk "github.com/darwinOrg/go-redis"
 	"github.com/rolandhe/saber/gocc"
 )
 
@@ -17,6 +19,17 @@ func TestAddJob(t *testing.T) {
 	})
 
 	time.Sleep(5 * time.Second)
+}
+
+func TestAddJobWithLock(t *testing.T) {
+	redisdk.InitClient("localhost:6379")
+	cron := NewAndStart(dglock.NewRedisLocker(true))
+	defer cron.Stop()
+	cron.AddJobWithLock("TestJob", "*/1 * * * * ?", 5000, func(ctx *dgctx.DgContext) {
+		fmt.Println(time.Now().UnixMilli())
+	})
+
+	time.Sleep(10 * time.Second)
 }
 
 func TestAddFixDurationJob(t *testing.T) {
